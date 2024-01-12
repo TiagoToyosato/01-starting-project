33e1a628f6e47835b94790266f428ce512fe836c
TiagoToyosato <toliveira_3@hotmail.com>
2024-01-12 11:03:49 +0000

    67 Component types dynamically

--- src/components/Tabs.jsx
+++ src/components/Tabs.jsx
@@ -1,7 +1,9 @@
-export default function Tabs({ children, buttons }) {
+export default function Tabs({ children, buttons, buttonsContainer }) {
+  // Need to start with capital letter/ the prop received could start with Capital letter but it against the padronization
+  const ButtonContainer = buttonsContainer;
   return (
     <>
-      <menu>{buttons}</menu>
+      <ButtonContainer>{buttons}</ButtonContainer>
       {children}
     </>
   );
