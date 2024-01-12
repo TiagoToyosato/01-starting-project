5ea3f41c63a75d632708a62a03fc0f0f484d05cc
TiagoToyosato <toliveira_3@hotmail.com>
2024-01-12 11:07:58 +0000

    68 Default props values

--- src/components/Tabs.jsx
+++ src/components/Tabs.jsx
@@ -1,4 +1,4 @@
-export default function Tabs({ children, buttons, buttonsContainer }) {
+export default function Tabs({ children, buttons, buttonsContainer = "menu" }) {
   // Need to start with capital letter/ the prop received could start with Capital letter but it against the padronization
   const ButtonContainer = buttonsContainer;
   return (
